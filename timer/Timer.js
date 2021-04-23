cabe5c050581364c760e0ef6432eef809fd410fd
MrCheezy1999 <ebb1999@live.com>
2021-04-23 11:02:06 -0500

    Done

---
+++ timer/Timer.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './Timer.css'
+
+const Timer = ({ setHide }) => {
+  const startingTime = 6;
+  const [time, setTime] = useState(startingTime);
+
+  const playGame = () => {
+    setTime(time - 1);
+    setHide(false);
+  }
+
+  useEffect(() => {
+    if(time !== startingTime){
+      setTimeout(() => {
+        if(time > 0){
+          setTime(time - 1);
+        } else {
+          setTime(startingTime);
+          setHide(true);
+          setTimeout(() => {
+            alert('TIME IS UP!!!');
+            setHide(false);
+          }, 100);
+        }
+      }, 1000);
+    }
+  }, [time]);
+
+  return(
+    <div className='Timer'>
+      <div className='Timer-Top'>
+        <p>TIMER</p>
+        <button onClick={playGame} className='Timer-Top-Btn'>PLAY</button>
+      </div>
+      <div className='Timer-Display'>
+        <h1 className='Timer-Display-H1'>{time}</h1>
+      </div>
+    </div>
+  );
+}
+export default Timer;
